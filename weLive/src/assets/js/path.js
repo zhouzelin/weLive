bda5846e74c97a0296b54bea1c767f4c80fe2ee4
zhouzelin <759005093@qq.com>
2017-10-16 15:20:40 +0800

    add files

---
+++ weLive/src/assets/js/path.js
@@ -0,0 +1,7 @@
+export const path_cofig = {
+	'home': '微邻',
+  'group': '圈子',
+  'discovery': '发现',
+  'user': '我的',
+  'userinfo': '个人信息'
+}
---
+++ weLive/src/components/user/userinfo.vue
@@ -0,0 +1,41 @@
+<template>
+	<div id="userinfo">
+		<v-header :header="header"></v-header>	
+	</div>
+</template>
+
+<script>
+	import VHeader from '@/components/header/header'
+
+	export default {
+		data() {
+			return {
+				header: {
+					title: '个人信息',
+					leftIcon: 'icon-arrow_lift',
+					backUrl: '/user'
+				}
+			}
+		},
+		mounted() {
+			
+		},
+		methods: {
+
+		},
+		components: {
+			VHeader
+		}
+	}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+		#userinfo
+			position absolute
+			top 0
+			bottom 0
+			left 0
+			width 100%
+			background #eee
+			z-index 2
+</style>
